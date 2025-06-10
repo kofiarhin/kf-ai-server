16960790e3729bada5112250c45727b09785beed
kofi arhin <kofiarhin69@gmail.com>
2025-06-10 02:48:41 +0100

    bug fix

--- server/routes/quizRoutes.js
+++ server/routes/quizRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const quizAi = require("../lib/quizAi");
+const baseAi = require("../lib/baseAi");
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const response = await quizAi();
+  const response = await baseAi();
 
   return res.json({ message: response });
 });
