ee19f5d13202fc50d10fabaccc60467b0369320c
kofi arhin <kofiarhin69@gmail.com>
2025-06-13 15:10:50 +0100

    update: added a default route for movie quiz ai

--- server/routes/movieQuizRoutes.js
+++ server/routes/movieQuizRoutes.js
@@ -2,6 +2,9 @@ const { Router } = require("express");
 const { getQuiz } = require("../conrtrollers/movieQuizController");
 const router = Router();
 
+router.get("/", (req, res) => {
+    return res.json{message: "welcome to the movie quiz app", slug: "i generate random quiz based on a movie"}
+})
 router.post("/", getQuiz);
 
 module.exports = router;
