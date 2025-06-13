e0438971c75ee4d20968631881945d00b6563896
kofi arhin <kofiarhin69@gmail.com>
2025-06-13 16:28:28 +0100

    Update: bug fix

--- server/routes/movieQuizRoutes.js
+++ server/routes/movieQuizRoutes.js
@@ -3,8 +3,11 @@ const { getQuiz } = require("../conrtrollers/movieQuizController");
 const router = Router();
 
 router.get("/", (req, res) => {
-    return res.json{message: "welcome to the movie quiz app", slug: "i generate random quiz based on a movie"}
-})
+  return res.json({
+    message: "welcome to the movie quiz app",
+    slug: "i generate random quiz based on a movie",
+  });
+});
 router.post("/", getQuiz);
 
 module.exports = router;
